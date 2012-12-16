72a81ad9601e30b2c785013240af573f7a262864
Dave Aitken <dave.aitken@gmail.com>
2012-12-16 10:26:12 +0000

    Tidied up a bit

--- Pacman/Graphics/Level.hs
+++ Pacman/Graphics/Level.hs
@@ -26,72 +26,14 @@ renderLevel lvl t = do
         gridCoords = map (zip xs . repeat) ys
 
     zipWithM_ (zipWithM_ (renderLevelItem t)) lvl gridCoords
-    --mapM_ (mapM_ renderOutlineDebug) gridCoords
-
---renderOutlineDebug :: (Float, Float) -> IO ()
---renderOutlineDebug (x, y) = do
---    let
---        points = [(x, y), (x, y+w), (x+w, y+w), (x+w, y), (x, y)]
---        verts = map pointToVertex points
---
---    color red 
---    renderPrimitive LineStrip $
---        mapM_ vertex verts
-
---level tile length
-l :: Float
-l = levelItemSize
-
---Wall thickness
-wT :: Float
-wT = 8
-
---Concave radius
-ccRa :: Float
-ccRa = 16
-
-ccWall :: (Float, Float) -> Float -> Float -> Float -> [(Float, Float)]
-ccWall (x, y) r t0 t1 = concat wallCurve where
-    wallCurve = map (map (\(u, v) -> (x + u, y + v))) curve
-    curve = foldr (\t acc -> [circlePt r t, circlePt (r + wT) t] : acc) [] tPoints
-    tPoints = [t0, t0 +  t1/12 .. t1]
-
-wall :: WallDirection -> [(Float, Float)]
-wall U    = scaleWall [(0, 16 - wT), (0, 16), (32, 16 - wT), (32, 16)]
-wall D    = scaleWall [(0, 16), (0, 16 + wT), (32, 16), (32, 16 + wT)]
-wall L    = scaleWall [(16, 0), (16, 32), (16 + wT, 0), (16 + wT, 32)]
-wall R    = scaleWall [(16 - wT, 0), (16 - wT, 32), (16, 0), (16, 32)]
-
-wall CcDL = scaleWall $ ccWall (0, 0) ccRa 0 (pi / 2)
-wall CcDR = scaleWall $ ccWall (32, 0) ccRa (pi / 2) pi
-wall CcUR = scaleWall $ ccWall (32, 32) ccRa pi (3 * pi / 2)
-wall CcUL = scaleWall $ ccWall (0, 32) ccRa (3 * pi / 2) (2 * pi)
-
-wall CvUL = scaleWall $ ccWall (32, 0) (ccRa - wT) (pi / 2) pi
-wall CvUR = scaleWall $ ccWall (0, 0) (ccRa - wT) 0 (pi / 2)
-wall CvDL = scaleWall $ ccWall (32, 32) (ccRa - wT) pi (3 * pi / 2)
-wall CvDR = scaleWall $ ccWall (0, 32) (ccRa - wT) (3 * pi / 2) (2 * pi)
-wall _    = scaleWall []
-
---wallColor CcUR = red
-wallColor _ = blue
-
-scaleWall :: [(Float, Float)] -> [(Float, Float)]
-scaleWall = map scaleWallPoint where
-                    scaleWallPoint (x, y) = (l * x / 32, l * y / 32)
-
-translateItem :: (Float, Float) -> [(Float, Float)] -> [(Float, Float)]
-translateItem dest = map (translatePoint dest) where 
-                       translatePoint (x, y) (x1, y1) = (x + x1, y + y1)
 
 renderLevelItem :: Float -> LevelItem  -> (Float, Float) -> IO ()
 renderLevelItem _ (Wall direction) dest = do
     let
-        points = translateItem dest $ wall direction
+        points = translatePts dest $ wall direction
         verts = map pointToVertex points
 
-    color (wallColor direction)
-    lineWidth $= 2
+    color blue
     renderPrimitive TriangleStrip $
         mapM_ vertex verts
 
@@ -101,12 +43,51 @@ renderLevelItem t (Pickup PowerPill) (x, y) = renderPill r_t (x, y) where
     
 renderLevelItem _ _ _ = return ()
 
+--level tile length
+l :: Float
+l = levelItemSize
+
+--Wall thickness
+wT :: Float
+wT = 8
+
+--Wall radius
+wR :: Float
+wR = 16
+
+ccWall :: (Float, Float) -> Float -> Float -> Float -> [(Float, Float)]
+ccWall gridTileDest r t0 t1 = concat wallCurve where
+    wallCurve = map (translatePts gridTileDest) curve
+    curve = foldr (\t acc -> [circlePt r t, circlePt (r + wT) t] : acc) [] tPoints
+    tPoints = [t0, t0 +  t1/12 .. t1]
+
+wall :: WallDirection -> [(Float, Float)]
+wall U    = scaleWall [(0, 16 - wT), (0, 16), (32, 16 - wT), (32, 16)]
+wall D    = scaleWall [(0, 16), (0, 16 + wT), (32, 16), (32, 16 + wT)]
+wall L    = scaleWall [(16, 0), (16, 32), (16 + wT, 0), (16 + wT, 32)]
+wall R    = scaleWall [(16 - wT, 0), (16 - wT, 32), (16, 0), (16, 32)]
+
+wall CcDL = scaleWall $ ccWall (0, 0) wR 0 (pi / 2)
+wall CcDR = scaleWall $ ccWall (32, 0) wR (pi / 2) pi
+wall CcUR = scaleWall $ ccWall (32, 32) wR pi (3 * pi / 2)
+wall CcUL = scaleWall $ ccWall (0, 32) wR (3 * pi / 2) (2 * pi)
+
+wall CvUL = scaleWall $ ccWall (32, 0) (wR - wT) (pi / 2) pi
+wall CvUR = scaleWall $ ccWall (0, 0) (wR - wT) 0 (pi / 2)
+wall CvDL = scaleWall $ ccWall (32, 32) (wR - wT) pi (3 * pi / 2)
+wall CvDR = scaleWall $ ccWall (0, 32) (wR - wT) (3 * pi / 2) (2 * pi)
+wall _    = scaleWall []
+
+scaleWall :: [(Float, Float)] -> [(Float, Float)]
+scaleWall = map scaleWallPoint where
+                    scaleWallPoint (x, y) = (l * x / 32, l * y / 32)
+
 renderPill :: Float -> (Float, Float) -> IO ()
 renderPill r dest = do
     let
         m = l/2.0
         points = map (\t -> (m + r * cos t, m + r * sin t)) [0, pi/8..2*pi]
-        positionedPoints = translateItem dest points
+        positionedPoints = translatePts dest points
 
         verts = map pointToVertex positionedPoints
 
--- Pacman/Util/Functions.hs
+++ Pacman/Util/Functions.hs
@@ -1,4 +1,12 @@
-module Pacman.Util.Functions (cyclingParam, circlePt) where
+module Pacman.Util.Functions (cyclingParam, circlePt, translatePts) where
+
+import Control.Arrow
+
+translate :: (Float, Float) -> (Float, Float) -> (Float, Float)
+translate (x, y) = (+ x) *** (+ y)
+
+translatePts :: (Float, Float) -> [(Float, Float)] -> [(Float, Float)]
+translatePts dst = map $ translate dst
 
 circlePt :: Float -> Float -> (Float, Float)
 circlePt r t = (r * cos t, r * sin t)
